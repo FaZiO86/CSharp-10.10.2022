89ab5fffcb670f9bc4658aea73c3ff75a9f7c1bd
FaZiO86 <91316222+FaZiO86@users.noreply.github.com>
2022-10-10 21:03:23 +0500

    исправил

--- CSharp 10.10.2022/Program.cs
+++ CSharp 10.10.2022/Program.cs
@@ -5,22 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using static System.Net.WebRequestMethods;
+using System.ComponentModel;
 
 namespace CSharp_10._10._2022 {
     internal class Program {
         static void Main(string[] args) {
-            int[] arr = new int[]{20,3,-5,-2,-10,-3,-7,15,5,4,6};
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+           
+
+            //int[] arr = new int[]{20,3,-5,-2,-10,-3,-7,15,5,4,6};
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            
             int summ = 0;
             var sw = new StreamWriter(path + "\\" + "number.txt");
-            for (int i = 0; i < arr.Length; i++) {
-                if (i%2 != 0 && (arr[i] < -2 | arr[i] %2 == 0)) {
-                    summ += arr[i];
-                    sw.WriteLine(arr[i]);
-                    Console.WriteLine(arr[i]);
+            for (int i = 0; i < args.Length; i++) {
+                if (i%2 != 0 && (int.Parse(args[i]) < -2 | int.Parse(args[i]) %2 == 0)) {
+                    summ += int.Parse(args[i]);
+                    sw.WriteLine(args[i]);
+                    Console.WriteLine(args[i]);
                 }
-
+            
             }
             sw.Close();
             Console.WriteLine("Сумма элементов равна: " + summ);
---
+++ start.cmd
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe %1
+dir *.exe
